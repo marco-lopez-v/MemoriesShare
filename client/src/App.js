ca8ddebd83f1d8f3cecf2d99946a81ae0a18ba3f
Marco Lopez <lopezvalmarco@gmail.com>
2021-11-26 18:32:27 -0800

    Finish first part of video

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts';
 import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
@@ -8,6 +10,11 @@ import useStyles from './styles';
 const App = () => {
 
     const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
 
     return (
         <Container maxWidth='lg'>
@@ -16,7 +23,7 @@ const App = () => {
                 <img className={classes.image} src={memories} alt='memories' height='60' />
             </AppBar>
             <Grow in>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                     <Grid item xs={2} sm={7}>
                         <Posts />
                     </Grid>
